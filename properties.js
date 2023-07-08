72024eb8495e75315c41a973c33c0748c26f4e60
wederson <wedersoncabral@gmail.com>
2023-07-08 18:15:36 -0300

    Add SVG properties

--- properties.js
+++ properties.js
@@ -10,6 +10,15 @@ define([], function () {
                 min: 1,
                 max: 15
             }, 
+            svg:    {
+                svgImage: {
+                    type: "string",
+                    ref: "svg.link",
+                    label: "SVG Link",
+                    defaultValue: './content/SVG_Template.svg',
+                    expression: "optional"
+                }
+            },
             appearance: {
                 uses: "settings"
             }
