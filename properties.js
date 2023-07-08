1f33892937382c780c666294475b1614209febd6
wederson <wedersoncabral@gmail.com>
2023-07-08 18:40:44 -0300

    Add properties to informe the holde placers

--- properties.js
+++ properties.js
@@ -8,7 +8,14 @@ define([], function () {
             measures: {
                 uses: "measures",
                 min: 1,
-                max: 15
+                max: 15,
+                measurePlaceHolder: {
+                    ref: "qDef.placeHolder.value",
+                    label: "Place Holder Value",
+                    type: "string",
+                    defaultValue: 'Measure_1',
+                    expression: "optional"
+                }
             }, 
             svg:    {
                 svgImage: {
